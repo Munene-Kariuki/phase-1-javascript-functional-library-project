4e859039d8e4a143b59b1cbcfa520cf186257d72
Denis <denismunene999@gmail.com>
2022-06-24 15:41:52 +0300

    commit tree 33a465a282ae259d0f2544a67767010cb7d0bc51

---
+++ index.js
@@ -0,0 +1,69 @@
+function myEach(collection, callback) {
+  if(Array.isArray(collection) === false) {
+    let newArray = Object.values(collection)
+    newArray.forEach((item) => callback(item))
+  } else {collection.forEach((item) => callback(item))}
+
+  return collection
+}
+
+function myMap(collection, callback) {
+  if(Array.isArray(collection) === false) {
+    return Object.values(collection).map((item) => callback(item))
+  } else {return collection.map((item) => callback(item))}
+}
+
+function checkArray(collection) {
+  if(Array.isArray(collection) === false) {
+    return Object.values(collection)
+  } else {return collection}
+}
+
+function myReduce(collection, callback, acc) {
+  let newArray = checkArray(collection)
+  if(! acc) {
+    acc = newArray[0]
+    newArray = newArray.slice(1)
+  }
+  
+  for(let i = 0; i < newArray.length; i++) {
+    acc = callback(acc, newArray[i], newArray)
+  }
+  return acc
+}
+
+function myFind(collection, predicate) {
+  let newCollection = checkArray(collection)
+
+  return newCollection.find((item) => predicate(item))
+}
+
+function myFilter(collection, predicate) {
+  let newCollection = checkArray(collection)
+
+  return newCollection.filter((item) => predicate(item))
+}
+
+function mySize(collection) {
+  let newCollection = checkArray(collection)
+
+  return newCollection.length
+}
+
+function myFirst(array, n = false) {
+
+  return (n) ? array.slice(0, n) : array[0]
+}
+
+function myLast(array, n = false) {
+
+  return (n) ? array.slice(array.length - n, array.length) : array[array.length - 1]
+}
+
+function myKeys(object) {
+  return Object.keys(object)
+}
+
+function myValues(object) {
+  return Object.values(object)
+}
